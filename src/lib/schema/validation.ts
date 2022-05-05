53a3cfd2e7301de01063d67b47f5c24c9dcadee4
Matt Osborn <mattjosborn@gmail.com>
2022-05-05 18:44:11 +0100

    page schema tests

--- src/lib/schema/validation.ts
+++ src/lib/schema/validation.ts
@@ -1,16 +1,21 @@
 import Ajv from "ajv";
+import type { AnyValidateFunction } from "ajv/dist/core";
 import * as schemaPage from "./page.json";
 
 export const ajv = new Ajv();
 
-ajv.addSchema(schemaPage, "page");
+ajv.addSchema(schemaPage);
 
-type Schemas = 'page';
+interface Validate {
+  (schema: string, data: any): void;
+  errors?: AnyValidateFunction<unknown>['errors'];
+}
 
-export const validate = (schema: Schemas, data: unknown) => {
+export const validate: Validate = (schema: string, data: unknown) => {
   const _validate = ajv.getSchema(schema);
   const valid = _validate(data);
   if (!valid) {
+    validate.errors = _validate.errors;
     throw new Error(_validate.errors.map(e => e.message).join(', '));
   }
 };

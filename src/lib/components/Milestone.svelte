cf83f479aa3b65a0e7f6689e59a2ecea20b40545
MKenz0 <miguel7vj@gmail.com>
2022-05-06 14:46:37 +0200

    Add contracted circle aspect

--- src/lib/components/Milestone.svelte
+++ src/lib/components/Milestone.svelte
@@ -35,9 +35,20 @@
               <path class="horizontal-path" d="M1.22933 0.955129V0.955129C2.81493 2.54068 5.01818 3.34773 7.25278 3.1615L9.729 2.95514"/>
             </svg>
           </div>
-          <svg class="milestone-circle" height="15" width="15">
-            <circle cx="7.5" cy="7.5" r="5" stroke="#FBE26B" stroke-width="3" fill={contracted[i] ? "#096EAE" : "#FBE26B"}/>
-          </svg>
+          {#if contracted[i]}
+            <svg class="milestone-circle-2" width="17" height="17" viewBox="0 0 17 17" fill="none">
+              <circle cx="8.5" cy="8.5" r="5" fill="#096EAE" stroke="#FBE26B" stroke-width="3"/>
+              <line x1="14" y1="0.5" x2="17" y2="0.5" stroke="#FBE26B"/>
+              <line x1="16.5" y1="3" x2="16.5" y2="1" stroke="#FBE26B"/>
+              <line x1="3" y1="16.5" x2="4.37114e-08" y2="16.5" stroke="#FBE26B"/>
+              <line x1="0.5" y1="14" x2="0.5" y2="16" stroke="#FBE26B"/>
+            </svg>
+          {:else}
+            <svg class="milestone-circle" height="15" width="15">
+              <circle cx="7.5" cy="7.5" r="5" stroke="#FBE26B" stroke-width="3" fill={contracted[i] ? "#096EAE" : "#FBE26B"}/>
+            </svg>
+          {/if}
+
           <div class={contracted[i] ? 'milestone-text contracted' : 'milestone-text expanded'}>
             <span>{m.text}</span>
           </div>
@@ -82,6 +93,10 @@
     transform: translate(17px, -6px);
   }
 
+  .milestone-circle-2 {
+    transform: translate(16px, -7px);
+  }
+
   .milestone-text {
     max-width: 200px;
     font-size: 16px;
@@ -89,6 +104,7 @@
   }
 
   .expanded {
+    padding-top: 1.5px;
     padding-left: 22px;
   }
 
@@ -111,7 +127,6 @@
   }
 
   .year {
-    font-family: 'Montserrat';
     font-size: 12px;
     color: #F9F9F9;
     padding-bottom: 10px;

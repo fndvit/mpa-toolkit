2d5a0672e3f87fe3648d47af907b3c659851aed3
Matt Osborn <mattjosborn@gmail.com>
2022-05-06 13:43:23 +0100

    add user img updating

---
+++ src/lib/components/content/UserImage.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { User } from "$lib/types";
+  import { staticUrl } from "$lib/helpers";
+
+  export let user: User;
+
+  $: src = user.img ? staticUrl(user.img) : '/missing-profile-photo.png';
+
+</script>
+
+<img class="user-image" on:click {src} alt={user.name} />
+
+<style>
+  img {
+    border-radius: 50%;
+    width: 66px;
+    height: 66px;
+  }
+</style>

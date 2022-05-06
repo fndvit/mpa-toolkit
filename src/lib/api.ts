298944c0ca3f343d89deeb799b37e09175d1f4e4
Matt Osborn <mattjosborn@gmail.com>
2022-05-06 13:42:24 +0100

    add clientside api fn

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,5 @@
+import type { User } from "$lib/types";
+import type { UserRequest } from "./types";
 
 export async function uploadImage(file: File) {
   const formData = new FormData();
@@ -8,4 +10,13 @@ export async function uploadImage(file: File) {
   });
   const body = await response.json() as {path: string};
   return body.path;
+}
+
+export async function updateUser(id: number, data: UserRequest) {
+  const response = await fetch(`/api/users/${id}`, {
+    method: 'PATCH',
+    body: JSON.stringify(data)
+  });
+  const { user } = await response.json() as {user: User};
+  return user;
 }

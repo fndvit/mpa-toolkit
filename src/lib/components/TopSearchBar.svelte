66cbb532b92cfb77336b89ea8ca3053a709a1985
MKenz0 <miguel7vj@gmail.com>
2022-05-13 09:06:32 +0200

    Search bar css

--- src/lib/components/TopSearchBar.svelte
+++ src/lib/components/TopSearchBar.svelte
@@ -7,14 +7,16 @@
 </script>
 
 <div class="container">
-
-  <input class="text" bind:value={search} placeholder={defaultText}>
+  <input class="text" bind:value={search}/>
+  {#if !search}
+    <div class="placeholder">Try <b>asking us</b> anything</div>
+  {/if}
 
   <div class="search-icon" on:click={searchButtonClicked}>
-      <svg class="search-icon" width="24" height="24" viewBox="0 0 24 24" fill="none">
-          <path class="search-path" d="M15.5 15.5L19 19" />
-          <path class="search-path" d="M5 11C5 14.3137 7.68629 17 11 17C12.6597 17 14.1621 16.3261 15.2483 15.237C16.3308 14.1517 17 12.654 17 11C17 7.68629 14.3137 5 11 5C7.68629 5 5 7.68629 5 11Z"/>
-      </svg>
+    <svg class="search-icon" viewBox="0 0 24 24">
+      <path class="search-path" d="M15.5 15.5L19 19" />
+      <path class="search-path" d="M5 11C5 14.3137 7.68629 17 11 17C12.6597 17 14.1621 16.3261 15.2483 15.237C16.3308 14.1517 17 12.654 17 11C17 7.68629 14.3137 5 11 5C7.68629 5 5 7.68629 5 11Z"/>
+    </svg>
   </div>
 </div>
 
@@ -36,15 +38,24 @@
     padding-left: 25px;
     padding-right: 25px;
     overflow: hidden;
+    display: flex;
+    justify-content: center;
   }
 
-  input::placeholder {
+  .placeholder {
     color: #FFFFFF;
+    position: absolute;
+    transform: translate(-15px, 10px);
+    pointer-events: none;
   }
 
   .search-icon {
     stroke-width: 1.5;
-    display: inline-block;
+    width:24px;
+    height:24px;
+    fill:none;
+    margin: auto;
+    margin-left: 5px;
     cursor: pointer;
   }
 

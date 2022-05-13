f2bf027a8cd3371cca5d1e36d67889f55065339f
MKenz0 <miguel7vj@gmail.com>
2022-05-13 08:41:26 +0200

    Remove unnecessary code on search bar

--- src/lib/components/TopSearchBar.svelte
+++ src/lib/components/TopSearchBar.svelte
@@ -2,14 +2,7 @@
 
   const defaultText = `Try asking us anything`;
   let search: string;
-  let strokeWidth = 1.5;
-  let writing = false;
-
   const searchButtonClicked = () => {}
-
-  const hovering = () => { strokeWidth = 2.25 }
-
-  const blurring = () => { strokeWidth = 1.5 }
 
 </script>
 
@@ -17,7 +10,7 @@
 
   <input class="text" bind:value={search} placeholder={defaultText}>
 
-  <div class="search-icon" on:click={searchButtonClicked} on:mouseenter={hovering} on:mouseleave={blurring}>
+  <div class="search-icon" on:click={searchButtonClicked}>
       <svg class="search-icon" width="24" height="24" viewBox="0 0 24 24" fill="none">
           <path class="search-path" d="M15.5 15.5L19 19" />
           <path class="search-path" d="M5 11C5 14.3137 7.68629 17 11 17C12.6597 17 14.1621 16.3261 15.2483 15.237C16.3308 14.1517 17 12.654 17 11C17 7.68629 14.3137 5 11 5C7.68629 5 5 7.68629 5 11Z"/>

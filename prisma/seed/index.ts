d56caf6b466a832c3d462968752d16d54c18781f
Matt Osborn <mattjosborn@gmail.com>
2022-05-24 00:53:25 +0100

    removing old commented code

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -1,16 +1,6 @@
 import { prisma } from "../../src/lib/prisma";
 import { seed } from "./seed";
 
-// async function main() {
-// }
 seed(true)
   .catch(console.error)
   .finally(async () => prisma.$disconnect());
-
-// export async function prod() {
-//   return run(false);
-
-// }
-// export async function dev() {
-//   return run(true);
-// }

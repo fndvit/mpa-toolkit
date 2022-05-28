3247f3d67150d2b39b5c7a61732d748a606a793c
Matt Osborn <mattjosborn@gmail.com>
2022-05-28 14:16:03 +0100

    pass schema to keymap plugin

--- src/lib/Editor/plugins.ts
+++ src/lib/Editor/plugins.ts
@@ -1,42 +1,39 @@
 import { keymap } from "prosemirror-keymap";
 import { baseKeymap } from "prosemirror-commands";
-import { history, redo, undo } from "prosemirror-history";
+import { history } from "prosemirror-history";
 import { dropCursor } from "prosemirror-dropcursor";
 import { gapCursor } from "prosemirror-gapcursor";
-
 import { richTextKeyMapPlugin } from "./keymap";
 import { Plugin } from "prosemirror-state";
-import { Decoration, DecorationSet } from "prosemirror-view";
 import { placeholderPlugin } from "./placeholder";
+import type { Schema } from "prosemirror-model";
+import type { DecorationSet } from "prosemirror-view";
 
-export { dropCursor, gapCursor, richTextKeyMapPlugin };
+export { dropCursor, gapCursor };
 
-/**
- * Core plugins which will be passed by default to each editor state instance
- * @type {*[]}
- */
+export const emptyPlugin = new Plugin<DecorationSet<Schema>>({
+  view: _view => {
+    const update = view => {
+      const isEmpty = view.state.doc.content.size === 0
+        || (view.state.doc.textContent === '' && view.state.doc.content.size < 3);
 
-const selectedDecoration = new Plugin({
-	props: {
-		decorations(state) {
-			const selection = state.selection;
-			const resolved = state.doc.resolve(selection.from);
-			const decoration = Decoration.node(resolved.before(1), resolved.after(1), {class: 'selected'});
-			return DecorationSet.create(state.doc, [decoration]);
-		}
-	}
+        view.dom.classList[isEmpty ? 'add' : 'remove']('ProseMirror-empty');
+    };
+
+    update(_view);
+    return { update };
+  }
 });
 
-export const corePlugins = [
-  history(),
-  keymap({"Mod-z": undo, "Mod-y": redo, "Mod-Shift-z": redo}),
-  keymap(baseKeymap),
-];
-
-export const richTextPlugins = [
-  dropCursor(),
-  gapCursor(),
-  richTextKeyMapPlugin,
-  selectedDecoration,
-  placeholderPlugin
-];
+export const plugins = (schema: Schema): Plugin[] => {
+  return [
+    history(),
+    // keymap({"Mod-z": undo, "Mod-y": redo, "Mod-Shift-z": redo}),
+    keymap(baseKeymap),
+    dropCursor(),
+    gapCursor(),
+    richTextKeyMapPlugin(schema),
+    emptyPlugin,
+    placeholderPlugin
+  ];
+};

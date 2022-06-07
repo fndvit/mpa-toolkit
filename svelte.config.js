6e845ec02e964a26730e939dbef39b3d4ca0bd15
Matt Osborn <mattjosborn@gmail.com>
2022-06-07 10:29:37 +0100

    add color mixins and refactor global scss

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,17 @@ import adapter from '@mpa-toolkit/adapter';
 import preprocess from 'svelte-preprocess';
 import svg from '@poppanator/sveltekit-svg'
 
+
+const globalSCSS = `
+@use "./src/lib/styles/color/mixins.scss" as *;
+@use "./src/lib/styles/typography/mixins.scss" as *;
+`;
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess({
     scss: {
-      prependData: `@use "./src/lib/styles/typography/mixins.scss" as *;`
+      prependData: globalSCSS
     }
   }),
 

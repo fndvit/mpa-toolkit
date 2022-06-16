17983e8853a9cfec2839c73f4832f1bcf5347bc9
u1956244 <58750500+u1956244@users.noreply.github.com>
2022-06-16 11:53:36 +0200

    new tag creation on update

--- src/routes/cms/tags.svelte
+++ src/routes/cms/tags.svelte
@@ -2,15 +2,16 @@
   import IconButton from '$lib/components/generic/IconButton.svelte';
   import Searchbar from '$lib/components/generic/Searchbar.svelte';
   import type { Tag } from '$lib/types';
+  import { TagType } from '@prisma/client';
   import EditableText from '$lib/components/generic/EditableText.svelte';
   import { getContext } from 'svelte';
   import type Toaster from '$lib/components/generic/Toaster.svelte';
   import DeleteModal from '$lib/components/cms/DeleteModal.svelte';
   import { openModal } from 'svelte-modals';
-import { createTag } from '$lib/api';
+  import { createTag } from '$lib/api';
+  import { updateTag } from '$lib/prisma/wrappers';
 
-
-  export let tags: (Tag & {_count: {pageTags: number}})[];
+  export let tags: (Tag & { _count: { pageTags: number } })[];
 
   let tagFocused = -1;
   let editableTag: EditableText;
@@ -18,34 +19,38 @@ import { createTag } from '$lib/api';
   const addToastMessage = getContext<Toaster['$$prop_def']['addMessage']>('addToastMessage');
 
   const addNewTag = () => {
-    createTag({
+    tags.push({
       value: 'New Tag',
-      type: 'TOPIC'
-    }).then((t) => {
-      addToastMessage('Tag created successfully');
-      console.log(t);
-    }).catch(err => {
-      addToastMessage('Error creating tag');
-      console.error('Error creating tag: ' + err);
+      type: 'TOPIC',
+      id: null,
+      _count: {
+        pageTags: 0
+      }
     });
+    tags = tags;
+    tagFocused = tags.length - 1;
+    addToastMessage('Tag created', { type: 'done'} );
   };
 
   const deleteTag = (i: number) => {
-    if(tags[i]._count.pageTags > 0){
+    if (tags[i]._count.pageTags > 0) {
       openModal(DeleteModal, {
         title: 'Delete Tag',
-        message: 'This tag is used on some pages. Are you sure you want to delete it? It will be removed from ' + tags[i]._count.pageTags + ' pages.',
+        message:
+          'This tag is used on some pages. Are you sure you want to delete it? It will be removed from ' +
+          tags[i]._count.pageTags +
+          ' pages.',
         confirmText: tags[i].value,
         onYes: () => {
           tags.splice(i, 1);
           tags = tags;
-          addToastMessage('Tag deleted', {type: 'done'});
+          addToastMessage('Tag deleted', { type: 'done' });
         }
       });
     } else {
       tags.splice(i, 1);
       tags = tags;
-      addToastMessage('Tag deleted', {type: 'done'});
+      addToastMessage('Tag deleted', { type: 'done' });
     }
   };
 
@@ -56,15 +61,20 @@ import { createTag } from '$lib/api';
   };
 
   const saveTag = (i: number) => {
-    if(tags[i].value.trim() === ''){
-      addToastMessage('Tag cannot be empty', {type: 'error'});
+    if (tags[i].value.trim() === '') {
+      addToastMessage('Tag cannot be empty', { type: 'error' });
       return;
     }
-    tags = tags;
-    tagFocused = -1;
-  }
-
-  $: console.log(editableTag)
+    (tags[i].id === null ? createTag({ value: tags[i].value, type: TagType.TOPIC}) : updateTag(tags[i].id, tags[i]))
+      .then((t) => {
+        addToastMessage('Tag updated successfully');
+      })
+      .catch((err) => {
+        addToastMessage('Error updating tag');
+      });
+    }
+  tags = tags;
+  tagFocused = -1;
 </script>
 
 <div class="tags-container">
@@ -73,28 +83,34 @@ import { createTag } from '$lib/api';
       <IconButton text="Add Tag" icon="add" on:click={addNewTag} />
     </div>
     <div class="tool-bar-item">
-      <Searchbar type='top'/>
+      <Searchbar type="top" />
     </div>
     <div class="sort-bar-item">
       <IconButton text="Sort" icon="sort" />
     </div>
-
   </div>
   <div class="tags-list">
-        {#each tags as tag, i}
-            <div class="tag">
-              <EditableText bind:value={tag.value} editable={true} placeholder="Tag name" on:focus={() => tagFocused = tag.id} on:focusout={() => {cancelTagEditon(tagFocused)}}/>
-              <p>- Frequency: {tag._count.pageTags}</p>
-              {#if tagFocused === tag.id}
-                <IconButton icon='done' on:click={() => saveTag(i)}/>
-                <IconButton icon='close' on:click={() => cancelTagEditon(i)}/>
-              {/if}
-                <div class="delete-year-button" on:click={() => deleteTag(i)}>
-                  <IconButton icon="delete"/>
-                </div>
-            </div>
-        {/each}
-
+    {#each tags as tag, i}
+      <div class="tag">
+        <EditableText
+          bind:value={tag.value}
+          editable={true}
+          placeholder="Tag name"
+          on:focus={() => (tagFocused = tag.id)}
+          on:focusout={() => {
+            cancelTagEditon(tagFocused);
+          }}
+        />
+        <p>- Frequency: {tag._count.pageTags}</p>
+        {#if tagFocused === tag.id}
+          <IconButton icon="done" on:click={() => saveTag(i)} />
+          <IconButton icon="close" on:click={() => cancelTagEditon(i)} />
+        {/if}
+        <div class="delete-year-button" on:click={() => deleteTag(i)}>
+          <IconButton icon="delete" />
+        </div>
+      </div>
+    {/each}
   </div>
 </div>
 

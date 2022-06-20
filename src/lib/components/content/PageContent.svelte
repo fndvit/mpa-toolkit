cca4ff7251e9312e90f0c588d849e6f20520b83d
MKenz0 <miguel7vj@gmail.com>
2022-06-20 09:53:10 +0200

    Set page context

--- src/lib/components/content/PageContent.svelte
+++ src/lib/components/content/PageContent.svelte
@@ -10,6 +10,7 @@
   import ContentCarousel from "$lib/components/content/ContentCarousel.svelte";
   import Image from "./Image.svelte";
   import LifeCycle from "../LifeCycle.svelte";
+  import { setContext } from "svelte/internal";
 
   export let page: SubTypes.Page.Full;
   export let recommendedPages: SubTypes.Page.ContentCard[] = null;
@@ -22,6 +23,8 @@
     'cards' : TextSlider,
     'image': Image,
   };
+
+  setContext('page-type', page.chapter ? 'chapter' : 'case-study');
 
   const headings = sections.filter(s => s.id);
 </script>

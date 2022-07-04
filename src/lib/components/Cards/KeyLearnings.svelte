1fa27d09a7ece56139a641f68b875744286bd8bf
MKenz0 <miguel7vj@gmail.com>
2022-07-04 12:39:45 +0200

    Fixed desktop width + unable click on already selected subject (KeyLearningsCards)

--- src/lib/components/Cards/KeyLearnings.svelte
+++ src/lib/components/Cards/KeyLearnings.svelte
@@ -12,19 +12,19 @@
   import IconButton from '$lib/components/generic/IconButton.svelte';
   import EditableText from "../generic/EditableText.svelte";
 
-  const example_card: KeyLearningsData[] = [ { subject: "What is needed", body: ['Budget efficiencies through use of technology','Effective local partnerships', 'Community engagement'] },
-  { subject: "What doesn't work", body: ['Budget efficiencies through use of technology 2','Effective local partnerships 2', 'Community engagement 2'] },
-  { subject: "What works", body: ['Budget efficiencies through use of technology 3','Effective local partnerships 3', 'Community engagement 3'] },
-  { subject: "What could work", body: ['Budget efficiencies through use of technology 4','Effective local partnerships 4', 'Community engagement 4'] },
-  { subject: "What will not work", body: ['Budget efficiencies through use of technology 5','Effective local partnerships 5', 'Community engagement 5'] },
-  { subject: "What you will do", body: ['Budget efficiencies through use of technology 6','Effective local partnerships 6', 'Community engagement 6'] },
-  { subject: "What you can't do", body: ['Budget efficiencies through use of technology 7','Effective local partnerships 7', 'Community engagement 7'] },
-  { subject: "What you must do", body: ['Budget efficiencies through use of technology 8','Effective local partnerships 8', 'Community engagement 8'] },
+  const example_card: KeyLearningsData[] = [ { subject: "What is needed", body: ['Mauris et metus sed mauris mattis tempor. Nulla pellentesque, diam in tincidunt consequat, nunc eros volutpat eros, eget rutrum urna nisl eu nunc.','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et metus sed mauris mattis tempor. Nulla pellentesque, diam in tincidunt consequat, nunc eros volutpat eros. Mauris et metus sed mauris mattis tempor. Nulla pellentesque, diam in tincidunt consequat, nunc eros volutpat eros. Sed elementum tempus odio at rutrum. Phasellus commodo neque eget tristique iaculis. Mauris quis mattis risus.', 'Mauris et metus sed mauris mattis tempor. Nulla pellentesque, diam in tincidunt consequat, nunc eros volutpat eros, eget rutrum urna nisl eu nunc. Aliquam a ullamcorper mauris. Nullam bibendum ut odio eget aliquet. Sed pretium malesuada sapien vel facilisis. Donec eu enim est.'] },
+  { subject: "What doesn't work", body: ['Quisque odio justo, porttitor et tincidunt nec, rutrum id felis. Vestibulum pulvinar volutpat ullamcorper. Mauris gravida purus eget lorem interdum vulputate. ','Maecenas id lacinia lectus, vel pharetra est. Vivamus sodales ipsum vel dictum porttitor. ', 'In sit amet dapibus arcu. Vestibulum nec mattis metus. Ut maximus id massa faucibus suscipit. Ut ut ornare arcu, id vestibulum urna.'] },
+  { subject: "What works", body: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum tempus odio at rutrum. Phasellus commodo neque eget tristique iaculis. Mauris quis mattis risus. Proin lacinia eu lacus ac euismod. ',' Mauris nec vestibulum nulla. Nam eu odio vel tellus condimentum faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.', 'Vestibulum quis urna nec odio ultricies blandit viverra imperdiet magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'] },
+  //{ subject: "What could work", body: ['Aenean eu turpis rhoncus, placerat urna ac, molestie arcu. Morbi tristique lectus eu sapien suscipit, ut pretium tortor accumsan. In ultrices nulla eu lacinia tempus.','Phasellus interdum mauris sit amet ipsum tincidunt aliquet. Vestibulum non varius nunc. Curabitur in mi ac justo luctus convallis.', 'Nullam bibendum ut odio eget aliquet. Sed pretium malesuada sapien vel facilisis. Donec eu enim est.'] },
+  //{ subject: "What will not work", body: ['Sed placerat urna euismod accumsan volutpat. Nulla eu libero purus. Aliquam semper libero in ante lacinia, interdum volutpat felis malesuada.','Vivamus fermentum ultricies lorem ac convallis. Sed placerat urna euismod accumsan volutpat. Nulla eu libero purus. Aliquam semper libero in ante lacinia, interdum volutpat felis malesuada. ', 'Aliquam semper libero in ante lacinia, interdum volutpat felis malesuada. Nullam porta neque felis, at ultrices felis accumsan sit amet. Curabitur nec est enim.'] },
+  //{ subject: "What you will do", body: [' Ut maximus id massa faucibus suscipit. Ut ut ornare arcu, id vestibulum urna. Praesent porttitor nisl in suscipit dignissim.','Nunc quis volutpat mi, at tincidunt ex. Proin feugiat suscipit tortor, at dignissim felis pharetra at. ', 'Phasellus luctus odio vitae velit aliquam consequat. Nulla non lorem semper risus commodo elementum in a nunc. Nunc quis volutpat mi, at tincidunt ex.'] },
+  //{ subject: "What you can't do", body: ['Sed lobortis tortor interdum libero mollis venenatis. Phasellus ultrices quam in ex pharetra luctus.','Quisque odio justo, porttitor et tincidunt nec, rutrum id felis. Vestibulum pulvinar volutpat ullamcorper.', 'Morbi tristique lectus eu sapien suscipit, ut pretium tortor accumsan. In ultrices nulla eu lacinia tempus.'] },
+  //{ subject: "What you must do", body: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum tempus odio at rutrum. Phasellus commodo neque eget tristique iaculis. Mauris quis mattis risus.','Vestibulum quis urna nec odio ultricies blandit viverra imperdiet magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Curabitur in mi ac justo luctus convallis. Sed sed mattis mauris, nec viverra leo. Nam vitae purus ante. Curabitur condimentum semper venenatis.'] },
 ];
 
   export let cards: KeyLearningsData[] = example_card;
   export let currentSubject = 0;
-  export let editable = true;
+  export let editable = false;
 
   let slideIndex: number = 0;
   let isTimerActive = true;
@@ -35,10 +35,12 @@
   }));
 
   const onClickChangeSubject = (n: number) => {
-    isTimerActive = false;
-    setTimeout(() => {isTimerActive = true}, 100);
-    currentSubject = n;
-    slideIndex = 0;
+    if (n !== currentSubject) {
+      isTimerActive = false;
+      setTimeout(() => {isTimerActive = true}, 100);
+      currentSubject = n;
+      slideIndex = 0;
+    }
   }
 
   const onClickAddKeyLearning = () => {
@@ -75,7 +77,7 @@
       </div>
 
     </div>
-    <div class="card-content no-heading">
+    <div class="card-content no-heading key-learnings">
       <Cards cards={slides} progress={isTimerActive} style='no-heading' {editable} bind:currentPageIndex={slideIndex}/>
     </div>
 
@@ -107,7 +109,7 @@
     margin: auto;
     top: 0px;
     height: 100%;
-    width: 1.25px;
+    width: 1px;
     background-color: $colors.neutral-light;
   }
 
@@ -146,6 +148,8 @@
   .navigation-menu {
     max-width: 220px;
     display: flex;
+    background-color: $colors.neutral-bg;
+    border-radius: 20px;
   }
 
   .titles-area {
@@ -173,7 +177,6 @@
     padding: 100px;
     grid-config(page, test);
 
-
     :global(.editable-text) {
       --outline-color: $colors.neutral-black;
       --caret-color: $colors.neutral-black;
@@ -190,23 +193,18 @@
   .card-content {
 
     :global(.cards){
-      width: 100%;
-      background-color: $colors.neutral-bg;
-      box-shadow: none;
-      height: 100%;
+      width: 750px;
     }
 
     :global(.splide){
       height: 100%;
-      margin-bottom: 100px;
     }
 
-    :global(.card-bottom){
-      position: absolute;
-      width: 100%;
-      bottom: 0;
-
-    }
+    //:global(.card-bottom){
+      //position: absolute;
+     // width: 100%;
+     // bottom: 0;
+    //}
 
     :global(.content){
       padding-top: 0px !important;

ae75d20dc1d323eef3722fcc1da3949c2841646d
Matt Osborn <mattjosborn@gmail.com>
2022-07-06 12:10:23 +0100

    refactor page endpoint

--- src/routes/[slug].ts
+++ src/routes/[slug].ts
@@ -3,12 +3,14 @@ import { getPageComponentProps } from "$lib/prisma/wrappers";
 
 export const get: RequestHandler<{slug: string}> = async ({ locals, params: { slug } }) => {
   const pageProps = await getPageComponentProps(slug, false);
-  const pageId = !('error' in pageProps.body) && pageProps.body.page.id;
-  const pageTags = !('error' in pageProps.body) && pageProps.body.page.tags;
 
-  if (pageId != null) locals.cacheKeys.add(`page-${pageId}`);
-  if (pageTags != null) pageTags?.forEach(tag => locals.cacheKeys.add(`tag-${tag.tag.id}`));
+  if ('error' in pageProps.body) return pageProps;
 
-  console.log('Page Props:', pageProps);
+  const pageId = pageProps.body.page.id;
+  const pageTags = pageProps.body.page.tags;
+
+  locals.cacheKeys.add(`page-${pageId}`);
+  pageTags.forEach(tag => locals.cacheKeys.add(`tag-${tag.tag.id}`));
+
   return pageProps;
 };

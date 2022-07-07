d05f5157db6e0f4ac8ce99b3a774aef61077a5b9
MKenz0 <miguel7vj@gmail.com>
2022-07-07 17:18:00 +0200

    Update KeyLearnings.svelte

--- src/lib/components/Cards/KeyLearnings.svelte
+++ src/lib/components/Cards/KeyLearnings.svelte
@@ -1,30 +1,40 @@
 <script lang="ts">
   import type { KeyLearningsData } from '$lib/types';
-  import Cards from './Cards.svelte';
+  import Cards, { CardData } from './Cards.svelte';
   import IconButton from '$lib/components/generic/IconButton.svelte';
   import EditableText from "../generic/EditableText.svelte";
 
-  const example_card: KeyLearningsData[] = [ { subject: "What is needed", body: ['Mauris et metus sed mauris mattis tempor. Nulla pellentesque, diam in tincidunt consequat, nunc eros volutpat eros, eget rutrum urna nisl eu nunc.','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et metus sed mauris mattis tempor. Nulla pellentesque, diam in tincidunt consequat, nunc eros volutpat eros. Mauris et metus sed mauris mattis tempor. Nulla pellentesque, diam in tincidunt consequat, nunc eros volutpat eros. Sed elementum tempus odio at rutrum. Phasellus commodo neque eget tristique iaculis. Mauris quis mattis risus.', 'Mauris et metus sed mauris mattis tempor. Nulla pellentesque, diam in tincidunt consequat, nunc eros volutpat eros, eget rutrum urna nisl eu nunc. Aliquam a ullamcorper mauris. Nullam bibendum ut odio eget aliquet. Sed pretium malesuada sapien vel facilisis. Donec eu enim est.'] },
-  { subject: "What doesn't work", body: ['Quisque odio justo, porttitor et tincidunt nec, rutrum id felis. Vestibulum pulvinar volutpat ullamcorper. Mauris gravida purus eget lorem interdum vulputate. ','Maecenas id lacinia lectus, vel pharetra est. Vivamus sodales ipsum vel dictum porttitor. ', 'In sit amet dapibus arcu. Vestibulum nec mattis metus. Ut maximus id massa faucibus suscipit. Ut ut ornare arcu, id vestibulum urna.'] },
-  { subject: "What works", body: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum tempus odio at rutrum. Phasellus commodo neque eget tristique iaculis. Mauris quis mattis risus. Proin lacinia eu lacus ac euismod. ',' Mauris nec vestibulum nulla. Nam eu odio vel tellus condimentum faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.', 'Vestibulum quis urna nec odio ultricies blandit viverra imperdiet magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'] },
-  { subject: "What could work", body: ['Aenean eu turpis rhoncus, placerat urna ac, molestie arcu. Morbi tristique lectus eu sapien suscipit, ut pretium tortor accumsan. In ultrices nulla eu lacinia tempus.','Phasellus interdum mauris sit amet ipsum tincidunt aliquet. Vestibulum non varius nunc. Curabitur in mi ac justo luctus convallis.', 'Nullam bibendum ut odio eget aliquet. Sed pretium malesuada sapien vel facilisis. Donec eu enim est.'] },
-  { subject: "What will not work", body: ['Sed placerat urna euismod accumsan volutpat. Nulla eu libero purus. Aliquam semper libero in ante lacinia, interdum volutpat felis malesuada.','Vivamus fermentum ultricies lorem ac convallis. Sed placerat urna euismod accumsan volutpat. Nulla eu libero purus. Aliquam semper libero in ante lacinia, interdum volutpat felis malesuada. ', 'Aliquam semper libero in ante lacinia, interdum volutpat felis malesuada. Nullam porta neque felis, at ultrices felis accumsan sit amet. Curabitur nec est enim.'] },
-  { subject: "What you will do", body: [' Ut maximus id massa faucibus suscipit. Ut ut ornare arcu, id vestibulum urna. Praesent porttitor nisl in suscipit dignissim.','Nunc quis volutpat mi, at tincidunt ex. Proin feugiat suscipit tortor, at dignissim felis pharetra at. ', 'Phasellus luctus odio vitae velit aliquam consequat. Nulla non lorem semper risus commodo elementum in a nunc. Nunc quis volutpat mi, at tincidunt ex.'] },
-  { subject: "What you can't do", body: ['Sed lobortis tortor interdum libero mollis venenatis. Phasellus ultrices quam in ex pharetra luctus.','Quisque odio justo, porttitor et tincidunt nec, rutrum id felis. Vestibulum pulvinar volutpat ullamcorper.', 'Morbi tristique lectus eu sapien suscipit, ut pretium tortor accumsan. In ultrices nulla eu lacinia tempus.'] },
-  { subject: "What you must do", body: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum tempus odio at rutrum. Phasellus commodo neque eget tristique iaculis. Mauris quis mattis risus.','Vestibulum quis urna nec odio ultricies blandit viverra imperdiet magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Curabitur in mi ac justo luctus convallis. Sed sed mattis mauris, nec viverra leo. Nam vitae purus ante. Curabitur condimentum semper venenatis.'] },
-];
+  const placeholders: KeyLearningsData[] = [ { subject: "What is needed", body: ['Mauris et metus sed mauris mattis tempor. Nulla pellentesque, diam in tincidunt consequat, nunc eros volutpat eros, eget rutrum urna nisl eu nunc.','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et metus sed mauris mattis tempor. Nulla pellentesque, diam in tincidunt consequat, nunc eros volutpat eros. Mauris et metus sed mauris mattis tempor. Nulla pellentesque, diam in tincidunt consequat, nunc eros volutpat eros. Sed elementum tempus odio at rutrum. Phasellus commodo neque eget tristique iaculis. Mauris quis mattis risus.', 'Mauris et metus sed mauris mattis tempor. Nulla pellentesque, diam in tincidunt consequat, nunc eros volutpat eros, eget rutrum urna nisl eu nunc. Aliquam a ullamcorper mauris. Nullam bibendum ut odio eget aliquet. Sed pretium malesuada sapien vel facilisis. Donec eu enim est.'] },
+    { subject: "What doesn't work", body: ['Quisque odio justo, porttitor et tincidunt nec, rutrum id felis. Vestibulum pulvinar volutpat ullamcorper. Mauris gravida purus eget lorem interdum vulputate. ','Maecenas id lacinia lectus, vel pharetra est. Vivamus sodales ipsum vel dictum porttitor. ', 'In sit amet dapibus arcu. Vestibulum nec mattis metus. Ut maximus id massa faucibus suscipit. Ut ut ornare arcu, id vestibulum urna.'] },
+    { subject: "What works", body: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum tempus odio at rutrum. Phasellus commodo neque eget tristique iaculis. Mauris quis mattis risus. Proin lacinia eu lacus ac euismod. ',' Mauris nec vestibulum nulla. Nam eu odio vel tellus condimentum faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.', 'Vestibulum quis urna nec odio ultricies blandit viverra imperdiet magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'] },
+    { subject: "What could work", body: ['Aenean eu turpis rhoncus, placerat urna ac, molestie arcu. Morbi tristique lectus eu sapien suscipit, ut pretium tortor accumsan. In ultrices nulla eu lacinia tempus.','Phasellus interdum mauris sit amet ipsum tincidunt aliquet. Vestibulum non varius nunc. Curabitur in mi ac justo luctus convallis.', 'Nullam bibendum ut odio eget aliquet. Sed pretium malesuada sapien vel facilisis. Donec eu enim est.'] },
+    { subject: "What will not work", body: ['Sed placerat urna euismod accumsan volutpat. Nulla eu libero purus. Aliquam semper libero in ante lacinia, interdum volutpat felis malesuada.','Vivamus fermentum ultricies lorem ac convallis. Sed placerat urna euismod accumsan volutpat. Nulla eu libero purus. Aliquam semper libero in ante lacinia, interdum volutpat felis malesuada. ', 'Aliquam semper libero in ante lacinia, interdum volutpat felis malesuada. Nullam porta neque felis, at ultrices felis accumsan sit amet. Curabitur nec est enim.'] },
+    { subject: "What you will do", body: [' Ut maximus id massa faucibus suscipit. Ut ut ornare arcu, id vestibulum urna. Praesent porttitor nisl in suscipit dignissim.','Nunc quis volutpat mi, at tincidunt ex. Proin feugiat suscipit tortor, at dignissim felis pharetra at. ', 'Phasellus luctus odio vitae velit aliquam consequat. Nulla non lorem semper risus commodo elementum in a nunc. Nunc quis volutpat mi, at tincidunt ex.'] },
+    { subject: "What you can't do", body: ['Sed lobortis tortor interdum libero mollis venenatis. Phasellus ultrices quam in ex pharetra luctus.','Quisque odio justo, porttitor et tincidunt nec, rutrum id felis. Vestibulum pulvinar volutpat ullamcorper.', 'Morbi tristique lectus eu sapien suscipit, ut pretium tortor accumsan. In ultrices nulla eu lacinia tempus.'] },
+    { subject: "What you must do", body: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum tempus odio at rutrum. Phasellus commodo neque eget tristique iaculis. Mauris quis mattis risus.','Vestibulum quis urna nec odio ultricies blandit viverra imperdiet magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Curabitur in mi ac justo luctus convallis. Sed sed mattis mauris, nec viverra leo. Nam vitae purus ante. Curabitur condimentum semper venenatis.'] },
+  ];
 
-  export let cards: KeyLearningsData[] = example_card;
+  const placeholder_example: KeyLearningsData = {subject: "", body: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit.']};
+
+  export let keyLearnings: KeyLearningsData[] = placeholders;
   export let currentSubject = 0;
   export let editable = false;
 
   let slideIndex: number = 0;
   let isTimerActive = true;
 
-  $: slides = cards[currentSubject].body.map(c=> ({
-    heading: null,
-    body: c
-  }));
+  let cards: CardData[] = [];
+
+  const loadCardStack = () => {
+    cards = keyLearnings[currentSubject].body.map(c=> ({
+      heading: null,
+      body: c
+    }));
+  }
+
+  loadCardStack();
+
+  $: keyLearnings[currentSubject].body = cards.map(c => c.body);
 
   const onClickChangeSubject = (n: number) => {
     if (n !== currentSubject) {
@@ -32,42 +42,51 @@
       setTimeout(() => {isTimerActive = true}, 100);
       currentSubject = n;
       slideIndex = 0;
+      loadCardStack();
     }
   }
 
   const onClickAddKeyLearning = () => {
-    let newKeyLearning: KeyLearningsData = {subject: '', body: ['']}
-    cards = [...cards, newKeyLearning];
-    onClickChangeSubject(cards.length-1);
+    let newKeyLearning: KeyLearningsData = placeholder_example;
+    keyLearnings = [...keyLearnings, newKeyLearning];
+    keyLearnings = keyLearnings;
+    onClickChangeSubject(keyLearnings.length-1);
   }
 
-  const onClickRemoveKeyLearning = () => {
-    cards.splice(currentSubject, 1);
-    cards = cards;
-    currentSubject = 0;
+  const onClickRemoveKeyLearning = (i: number) => {
+    //keyLearnings.splice(i, 1);
+    //keyLearnings = keyLearnings;
+  }
+
+  const click = () => {
+    console.log("CARDS -> ", cards);
+    console.log("KEY LEARNINGS -> ", keyLearnings);
   }
 
 </script>
+
+<button on:click={click}>JAJA</button>
 
 <div class="container">
   <div class="card">
     <div class="navigation-menu">
       <div class="titles-area">
-        {#each cards as card, i}
+        {#each keyLearnings as k, i}
           <div class="title" id={i.toString()} class:selected={i===currentSubject} on:click={() => onClickChangeSubject(i)}>
-            <EditableText bind:value={card.subject} {editable} placeholder='Key learning...' />
+            <EditableText bind:value={k.subject} {editable} placeholder='Key learning...' />
           </div>
         {/each}
         {#if editable}
           <div class="editor-button">
             <IconButton icon="add" on:click={onClickAddKeyLearning} />
+            <IconButton icon="delete" on:click={() => onClickRemoveKeyLearning(currentSubject)} />
           </div>
         {/if}
       </div>
 
       <div class="middle-area">
         <div class="vertical-dots">
-          {#each cards as {}, i}
+          {#each keyLearnings as {}, i}
             <div class="circle" class:selected={i===currentSubject} on:click={() => onClickChangeSubject(i)}/>
           {/each}
         </div>
@@ -76,7 +95,14 @@
     </div>
 
     <div class="card-content no-heading key-learnings">
-      <Cards cards={slides} handleDeletion={onClickRemoveKeyLearning} progress={isTimerActive} style='no-heading' {editable} bind:currentPageIndex={slideIndex}/>
+      <Cards
+        bind:cards
+        canToggleHeading={false}
+        progress={isTimerActive}
+        style='no-heading'
+        {editable}
+        bind:currentPageIndex={slideIndex}
+      />
     </div>
   </div>
 </div>
@@ -91,6 +117,10 @@
 
   .editor-button {
     margin-top: 10px;
+    right: 40px;
+    display: flex;
+    column-gap: 5px;
+    justify-content: left;
     :global(.icon-button) {
       --ib-icon-bg: #00000022;
       --ib-hover-bg: #00000022;
@@ -205,7 +235,7 @@
 
     //:global(.card-bottom) {
       //position: absolute;
-     // width: 100%;
+      //width: 100%;
       //bottom: 0;
     //}
   }
@@ -267,10 +297,6 @@
       width: 100%;
       transform: translateY(-5px);
     }
-
-  }
-
-  +breakpoint(page, small){
 
   }
 

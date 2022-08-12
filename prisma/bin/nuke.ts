671037a80392f04b3213427f28fcec3eb7d8bcc0
Matt Osborn <mattjosborn@gmail.com>
2022-08-12 01:57:25 +0100

    better error handling for nuke

--- prisma/bin/nuke.ts
+++ prisma/bin/nuke.ts
@@ -1,15 +1,26 @@
 import chalk from 'chalk';
 import { prisma } from '../../src/lib/prisma';
-import { reset } from "../lib/reset";
-import { seed } from "../lib/seed";
+import { reset } from '../lib/reset';
+import { seed } from '../lib/seed';
 
 async function main() {
-
   console.log(chalk.green('Resetting database...'));
-  await reset();
+  try {
+    await reset();
+  } catch (e) {
+    console.log(chalk.red('Error resetting database:'));
+    console.log(e);
+    process.exit(1);
+  }
 
   console.log(chalk.green('Seeding database...'));
-  await seed(true);
+  try {
+    await seed(true);
+  } catch (e) {
+    console.log(chalk.red('Error seeding database:'));
+    console.log(e);
+    process.exit(1);
+  }
 }
 
 main()

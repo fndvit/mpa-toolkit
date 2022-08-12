df4a589c048e4e49f3cd728a55561f5a35eaa6e9
Matt Osborn <mattjosborn@gmail.com>
2022-08-12 01:57:05 +0100

    pretty reset

--- prisma/lib/reset.ts
+++ prisma/lib/reset.ts
@@ -1,42 +1,32 @@
 import { prisma } from '../../src/lib/prisma';
-import { execFile } from "child_process";
-import * as path from "path";
+import { execFile } from 'child_process';
+import * as path from 'path';
 
 async function dropAllFunctions() {
-  const functionNames = await prisma.$queryRaw<{ name: string }[]>
-    `SELECT routine_name as name FROM information_schema.routines
+  const functionNames = await prisma.$queryRaw<
+    { name: string }[]
+  >`SELECT routine_name as name FROM information_schema.routines
      WHERE routine_type = 'FUNCTION' AND routine_schema = 'public'`;
 
   return prisma.$transaction(
-    functionNames.map(({ name }) =>
-      prisma.$executeRawUnsafe(`DROP FUNCTION IF EXISTS ${name} CASCADE;`)
-    )
+    functionNames.map(({ name }) => prisma.$executeRawUnsafe(`DROP FUNCTION IF EXISTS ${name} CASCADE;`))
   );
 }
 
 async function prismaCmd(cmd: string) {
-  return new Promise((resolve) => {
-    execFile(
-      'node',
-      [
-        path.resolve("./node_modules/prisma/build/index.js"),
-        ...cmd.split(' ')
-      ],
-      (error, stdout) => {
-        if (error != null) {
-          console.log(stdout);
-          console.error(`prisma exited with error ${error.message}`);
-          resolve(error.code ?? 1);
-        } else {
-          resolve(0);
-        }
-      },
-    );
+  return new Promise((resolve, reject) => {
+    execFile('node', [path.resolve('./node_modules/prisma/build/index.js'), ...cmd.split(' ')], (error, stdout) => {
+      if (error != null) {
+        console.log(stdout);
+        reject(error);
+      } else {
+        resolve(0);
+      }
+    });
   });
 }
 
 export async function reset() {
-
   console.log('Generating prisma client files...');
   await prismaCmd('generate');
 
@@ -45,5 +35,4 @@ export async function reset() {
 
   console.log('Running prisma reset...');
   await prismaCmd('migrate reset --force');
-
 }

5bb2627b2a6de283a7080d5a804441e0afef6e5d
Matt Osborn <mattjosborn@gmail.com>
2022-08-19 01:41:57 +0100

    update ignore files and lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,34 @@
-module.exports = {
+// @ts-check
+const { defineConfig } = require('eslint-define-config');
+
+module.exports = defineConfig({
   root: true,
   parser: '@typescript-eslint/parser',
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  plugins: ['svelte3', '@typescript-eslint'],
+  plugins: ['svelte3', '@typescript-eslint', 'import'],
   ignorePatterns: ['*.cjs'],
   overrides: [
     {
       files: ['*.svelte'],
       processor: 'svelte3/svelte3',
       rules: {
-        "semi": [2, "always"],
+        semi: [2, 'always']
       }
     },
     {
       files: ['*.ts'],
       rules: {
-        "semi": [2, "always"],
+        semi: [2, 'always']
       }
     }
   ],
   rules: {
-    "no-undef": "off",
-    "@typescript-eslint/no-namespace": "off"
+    'no-undef': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports', disallowTypeAnnotations: false }],
+    'import/no-duplicates': 'error',
+    'import/order': 'error'
   },
   settings: {
     'svelte3/typescript': () => require('typescript')
@@ -35,4 +42,4 @@ module.exports = {
     es2017: true,
     node: true
   }
-};
+});

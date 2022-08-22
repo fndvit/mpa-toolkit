3788a0542f9f5241a31f5585ef3a8cfbefea0f35
Matt Osborn <mattjosborn@gmail.com>
2022-08-22 10:14:22 +0100

    fix global stylus path for windows

--- packages/web/svelte.config.js
+++ packages/web/svelte.config.js
@@ -1,12 +1,12 @@
 // @ts-check
+import path from 'path';
 import adapter from '@mpa/adapter';
-// import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
-const globalStylePath = new URL('./src/lib/styles/svelte-global', import.meta.url);
+const globalStylePath = path.resolve(process.cwd(), './src/lib/styles/svelte-global');
 
 const globalStylus = `
-  @require '${globalStylePath.pathname}';
+  @require '${globalStylePath}';
 `;
 
 /** @type {import('@sveltejs/kit').Config} */

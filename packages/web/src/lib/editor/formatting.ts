ae26c271293c5be28d4247292987c795f708b04d
Matt Osborn <mattjosborn@gmail.com>
2022-08-23 21:16:17 +0100

    formatting toolbar widget

--- packages/web/src/lib/editor/formatting.ts
+++ packages/web/src/lib/editor/formatting.ts
@@ -3,21 +3,7 @@ import { Plugin } from 'prosemirror-state';
 import { Decoration, DecorationSet } from 'prosemirror-view';
 import type { NodeName } from './schema';
 
-const NODE_RULES: { [k in NodeName]?: NodeRule[] } = {};
-
-addNodeRule('heading', 'warning', 'Headings should not start with a space', node =>
-  /^(\s+)/.exec(node.textContent) ? 'warning' : false
-);
-
-addNodeRule('paragraph', 'warning', 'Paragraphs should not start with a space', node =>
-  /^(\s+)/.exec(node.textContent) ? 'warning' : false
-);
-
-addNodeRule('heading', 'error', 'Main sections need "read more" button text', node =>
-  node.attrs.level === 1 && node.attrs.showmore?.length === 0 ? 'error' : false
-);
-
-type FormattingError = {
+export type FormattingError = {
   rule: NodeRule;
   from: number;
   to: number;
@@ -32,53 +18,65 @@ type NodeRule = {
   check: (node: Node) => unknown;
 };
 
-function addNodeRule(
-  nodeTypes: NodeName | NodeName[],
-  level: ErrorLevel,
-  msg: NodeRule['msg'],
-  check: NodeRule['check']
-) {
-  if (!Array.isArray(nodeTypes)) {
-    addNodeRule([nodeTypes], level, msg, check);
-  } else {
-    nodeTypes.forEach(nodeType => {
-      NODE_RULES[nodeType] = NODE_RULES[nodeType] ?? [];
-      NODE_RULES[nodeType].push({ nodeTypes, msg, level, check });
+class FormattingPlugin extends Plugin<{ decorations: DecorationSet; problems: FormattingError[] }> {
+  rules: { [k in NodeName]?: NodeRule[] } = {};
+  lastResult: FormattingError[];
+
+  constructor() {
+    super({
+      state: {
+        init: (_, { doc }) => this.runFormatting(doc),
+        apply: (tr, old) => (tr.docChanged ? this.runFormatting(tr.doc) : old)
+      },
+      props: {
+        decorations: state => this.getState(state).decorations
+      }
     });
   }
-}
 
-function formattingDeco(doc: Node) {
-  const decos = checkFormatting(doc).map(prob =>
-    Decoration.inline(prob.from, prob.to, { class: 'problem', 'data-hover-msg': prob.rule.msg })
-  );
-  return DecorationSet.create(doc, decos);
-}
+  runFormatting(doc: Node) {
+    const problems = this.checkFormatting(doc);
+    const decos = problems.map(prob =>
+      Decoration.inline(prob.from, prob.to, { class: 'problem', 'data-hover-msg': prob.rule.msg })
+    );
+    return { decorations: DecorationSet.create(doc, decos), problems };
+  }
 
-function checkFormatting(doc: Node) {
-  const result: FormattingError[] = [];
-  const record = (rule: NodeRule, from: number, to: number) => result.push({ rule, from, to });
-  doc.descendants((node, pos) => {
-    const rules = NODE_RULES[node.type.name as NodeName];
-    if (rules) {
-      rules.filter(rule => rule.check(node)).forEach(rule => record(rule, pos, pos + node.nodeSize));
-    }
-  });
-  return result;
-}
+  checkFormatting(doc: Node) {
+    const result: FormattingError[] = [];
+    const record = (rule: NodeRule, from: number, to: number) => result.push({ rule, from, to });
+    doc.descendants((node, pos) => {
+      const rules = this.rules[node.type.name as NodeName];
+      if (rules) {
+        rules.filter(rule => rule.check(node)).forEach(rule => record(rule, pos, pos + node.nodeSize));
+      }
+    });
+    this.lastResult = result;
+    return result;
+  }
 
-export const formattingPlugin = new Plugin({
-  state: {
-    init(_, { doc }) {
-      return formattingDeco(doc);
-    },
-    apply(tr, old) {
-      return tr.docChanged ? formattingDeco(tr.doc) : old;
-    }
-  },
-  props: {
-    decorations(state) {
-      return this.getState(state);
+  addNodeRule(nodeTypes: NodeName | NodeName[], level: ErrorLevel, msg: NodeRule['msg'], check: NodeRule['check']) {
+    if (!Array.isArray(nodeTypes)) {
+      this.addNodeRule([nodeTypes], level, msg, check);
+    } else {
+      nodeTypes.forEach(nodeType => {
+        this.rules[nodeType] = this.rules[nodeType] ?? [];
+        this.rules[nodeType].push({ nodeTypes, msg, level, check });
+      });
     }
   }
-});
+}
+
+export const formattingPlugin = new FormattingPlugin();
+
+formattingPlugin.addNodeRule('heading', 'warning', 'Headings should not start with a space', node =>
+  /^(\s+)/.exec(node.textContent) ? 'warning' : false
+);
+
+formattingPlugin.addNodeRule('paragraph', 'warning', 'Paragraphs should not start with a space', node =>
+  /^(\s+)/.exec(node.textContent) ? 'warning' : false
+);
+
+formattingPlugin.addNodeRule('heading', 'error', 'Main sections need "read more" button text', node =>
+  node.attrs.level === 1 && node.attrs.showmore?.length === 0 ? 'error' : false
+);

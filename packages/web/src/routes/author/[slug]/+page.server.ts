18125a8a39e5e441f3b3ab62391fbeb47c3554b5
MKenz0 <miguel7vj@gmail.com>
2022-08-23 15:49:11 +0200

    Change author [id] into -> author [slug]

--- packages/web/src/routes/author/[slug]/+page.server.ts
+++ packages/web/src/routes/author/[slug]/+page.server.ts
@@ -1,10 +1,26 @@
 import { Queries } from '@mpa/db';
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { slugify } from '$lib/utils';
 import { db } from '$lib/db';
 
+async function getAuthorBySlug(slug: string) {
+  // can't directly query db by slug using prisma
+  // so doing the query client-side
+  const allAuthors = await db.prisma.author.findMany({
+    select: { id: true, name: true}
+  });
+  const author = allAuthors.find(a => slugify(a.name) === slug);
+  return author?.id;
+}
+
 export const load: PageServerLoad = async ({ params, locals }) => {
-  const id = parseInt(params.id);
+
+  const slug = params.slug.toLowerCase();
+
+  if (slug !== params.slug) throw redirect(301, `/author/${slug}`);
+
+  const id = await getAuthorBySlug(slug);
 
   if (isNaN(id)) throw error(404, 'Page not found');
 
--- packages/web/src/routes/author/[slug]/+page.svelte
+++ packages/web/src/routes/author/[slug]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import type { PageData } from './$types';
+  import type { PageData } from '../../../../.svelte-kit/types/src/routes/author/[slug]/$types';
   import { CollectionPage } from '$lib/components';
 
   export let data: PageData;

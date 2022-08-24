fb00758161e2daecd17571530122dc109e5ced04
Matt Osborn <mattjosborn@gmail.com>
2022-08-24 10:05:41 +0100

    remove intermediate variable

--- packages/web/src/routes/recommended/[slug]/+page.server.ts
+++ packages/web/src/routes/recommended/[slug]/+page.server.ts
@@ -38,7 +38,7 @@ export const load: PageServerLoad = async ({ locals, params }) => {
     ...Queries.pageForCollectionCard
   });
 
-  const sortedPages = unorderedPages
+  const pages = unorderedPages
     .map(page => ({
       numTags: page.tags.filter(t => ids.includes(t.tag.id)).length,
       page
@@ -47,9 +47,8 @@ export const load: PageServerLoad = async ({ locals, params }) => {
       if (a.numTags > b.numTags) return -1;
       else if (a.numTags < b.numTags) return 1;
       return a.page.createdAt.getTime() - b.page.createdAt.getTime();
-    });
-
-  const pages = sortedPages.map(op => op.page);
+    })
+    .map(p => p.page);
 
   pages.map(p => p.tags.map(t => locals.cacheKeys.add(`tag-${t.tag.id}`)));
   locals.cacheKeys.add('pages');

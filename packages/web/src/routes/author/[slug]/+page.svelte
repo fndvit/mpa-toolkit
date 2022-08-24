abc14f0d84a0441e8bc08b4baaf83265cc4d15dd
Matt Osborn <mattjosborn@gmail.com>
2022-08-24 10:20:39 +0100

    fix broken import

--- packages/web/src/routes/author/[slug]/+page.svelte
+++ packages/web/src/routes/author/[slug]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { PageData } from '../../../../.svelte-kit/types/src/routes/author/[slug]/$types';
-  import { CollectionPage } from '$lib/components';
+  import CollectionPage from '$lib/components/collection/CollectionPage.svelte';
 
   export let data: PageData;
 </script>

a73b7b3550382953ec5f38adfada2cea17f21cdc
LeVarez <aleixac1997@gmail.com>
2022-09-02 12:22:24 +0200

    get X tags most used

--- packages/web/src/routes/+page.server.ts
+++ packages/web/src/routes/+page.server.ts
@@ -3,6 +3,8 @@ import { groupBy } from '@mpa/utils';
 import type { PageServerLoad } from './$types';
 import { db } from '$lib/db';
 
+const NUM_TAGS = 10; //number of tags showed in the serach component
+
 export const load: PageServerLoad = async ({ locals }) => {
   const pages = await db.prisma.page.findMany({
     where: { draft: false },
@@ -11,7 +13,8 @@ export const load: PageServerLoad = async ({ locals }) => {
 
   const tags = await db.prisma.tag.findMany({
     where: { type: 'TOPIC' },
-    ...Queries.tag
+    ...Queries.countTags,
+    orderBy: { pageTags: {_count: 'desc'} }
   });
 
   const components = await db.homepage.getComponents();
@@ -22,5 +25,5 @@ export const load: PageServerLoad = async ({ locals }) => {
 
   const groups = groupBy(pages, p => (p.chapter ? 'chapters' : 'caseStudies'));
 
-  return { chapters: groups.chapters || [], caseStudies: groups.caseStudies || [], tags, components };
+  return { chapters: groups.chapters || [], caseStudies: groups.caseStudies || [], tags: tags.slice(0, NUM_TAGS), components };
 };

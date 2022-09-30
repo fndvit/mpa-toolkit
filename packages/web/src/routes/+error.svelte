72e40883704c0e19f706952c37b20ac768ddc47a
Matt Osborn <mattjosborn@gmail.com>
2022-09-30 18:46:43 -0500

    fix formatting

--- packages/web/src/routes/+error.svelte
+++ packages/web/src/routes/+error.svelte
@@ -5,10 +5,21 @@
 <div class="container">
   <h1>{$page.status} error</h1>
   {#if $page.status === 404}
-    <p>Erm ...<br>That's a bit awkward ...<br>But ...<br>We couldn't find that page<br>Try going to <a href="/">MPAth's home page</a>.</p>
+    <p>
+      Erm ...<br />
+      That's a bit awkward ...<br />
+      But ...<br />
+      We couldn't find that page<br />
+      Try going to <a href="/">MPAth's home page</a>.
+    </p>
   {:else}
     <div class="inner">
-      <p>Ooops!!<br>This one's one us <br>— something's not right on our side.<br>Refresh your browser or try us later maybe?</p>
+      <p>
+        Ooops!!<br />
+        This one's one us <br />
+        — something's not right on our side.<br />
+        Refresh your browser or try us later maybe?
+      </p>
     </div>
   {/if}
 </div>

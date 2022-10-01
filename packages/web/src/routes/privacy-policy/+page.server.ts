2ee67250d0dfc1cd5d0d935efd78ff77f873e483
Matt Osborn <mattjosborn@gmail.com>
2022-09-30 19:23:42 -0500

    prettier and linting fixes

--- packages/web/src/routes/privacy-policy/+page.server.ts
+++ packages/web/src/routes/privacy-policy/+page.server.ts
@@ -2,10 +2,10 @@ import { marked } from 'marked';
 import type { PageServerLoad } from './$types';
 import privacy from '$lib/about/privacy.md?raw';
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: PageServerLoad = async () => {
   let content = marked.parse(privacy);
-  let lastUpdated = content.split("</p>\n").shift();
-  content = content.replace(lastUpdated, '').replace("</p>\n", '');
-  lastUpdated = lastUpdated.replace("<p>",'');
-  return {content, lastUpdated};
+  let lastUpdated = content.split('</p>\n').shift();
+  content = content.replace(lastUpdated, '').replace('</p>\n', '');
+  lastUpdated = lastUpdated.replace('<p>', '');
+  return { content, lastUpdated };
 };
--- packages/web/src/routes/terms-of-use/+page.server.ts
+++ packages/web/src/routes/terms-of-use/+page.server.ts
@@ -2,10 +2,10 @@ import { marked } from 'marked';
 import type { PageServerLoad } from './$types';
 import terms from '$lib/about/terms.md?raw';
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: PageServerLoad = async () => {
   let content = marked.parse(terms);
-  let lastUpdated = content.split("</p>\n").shift();
-  content = content.replace(lastUpdated, '').replace("</p>\n", '');
-  lastUpdated = lastUpdated.replace("<p>",'');
-  return {content, lastUpdated};
+  let lastUpdated = content.split('</p>\n').shift();
+  content = content.replace(lastUpdated, '').replace('</p>\n', '');
+  lastUpdated = lastUpdated.replace('<p>', '');
+  return { content, lastUpdated };
 };

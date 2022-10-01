f8f064196ff848ceeec1a9e19a30e3cdf9cd77b8
Matt Osborn <mattjosborn@gmail.com>
2022-09-30 19:23:17 -0500

    rename md files

--- packages/web/src/routes/privacy-policy/+page.server.ts
+++ packages/web/src/routes/privacy-policy/+page.server.ts
@@ -1,9 +1,9 @@
 import { marked } from 'marked';
 import type { PageServerLoad } from './$types';
-import privacyPolicy from '$lib/about/privacyPolicy.md?raw';
+import privacy from '$lib/about/privacy.md?raw';
 
 export const load: PageServerLoad = async ({ locals }) => {
-  let content = marked.parse(privacyPolicy);
+  let content = marked.parse(privacy);
   let lastUpdated = content.split("</p>\n").shift();
   content = content.replace(lastUpdated, '').replace("</p>\n", '');
   lastUpdated = lastUpdated.replace("<p>",'');
--- packages/web/src/routes/terms-of-use/+page.server.ts
+++ packages/web/src/routes/terms-of-use/+page.server.ts
@@ -1,9 +1,9 @@
 import { marked } from 'marked';
 import type { PageServerLoad } from './$types';
-import termsOfUse from '$lib/about/termsOfUse.md?raw';
+import terms from '$lib/about/terms.md?raw';
 
 export const load: PageServerLoad = async ({ locals }) => {
-  let content = marked.parse(termsOfUse);
+  let content = marked.parse(terms);
   let lastUpdated = content.split("</p>\n").shift();
   content = content.replace(lastUpdated, '').replace("</p>\n", '');
   lastUpdated = lastUpdated.replace("<p>",'');

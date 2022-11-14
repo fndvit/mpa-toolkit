41a133d8659a8220c776c6a86d0b787b3f0f11ad
Matt Osborn <mattjosborn@gmail.com>
2022-11-13 22:53:01 -0500

    remove debug logging

--- packages/env/local.ts
+++ packages/env/local.ts
@@ -3,5 +3,4 @@ import * as dotenv from 'dotenv';
 
 const envFile = process.env.TEST ? '.env.test' : '.env';
 
-console.log('loading env from', envFile);
 dotenv.config({ path: path.join(process.cwd(), '../..', envFile) });

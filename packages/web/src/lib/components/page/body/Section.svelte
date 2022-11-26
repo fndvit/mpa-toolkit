cb867478893611be58ab41b44cbddbb167557249
Matt Osborn <mattjosborn@gmail.com>
2022-11-26 16:29:27 -0600

    fix scroll on section collapse

--- packages/web/src/lib/components/page/body/Section.svelte
+++ packages/web/src/lib/components/page/body/Section.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { Section, CollapseBlock } from '@mpa/db';
+  import { tick } from 'svelte';
   import ExpandButton from './ExpandButton.svelte';
   export let section: Section;
 
@@ -8,13 +9,23 @@
   const totalBlocks = section.blocks.length - 1;
   const index = section.blocks.findIndex((_, i) => i > 0 && i < totalBlocks && section.blocks[i].type === 'collapse');
   let showmore = index !== -1 ? (section.blocks[index] as CollapseBlock).attrs.showmore : '';
+
+  const onClickExpand = async (e: Event) => {
+    const buttonEl = e.currentTarget as HTMLButtonElement;
+    const offsetTop = buttonEl.offsetTop;
+    expanded = !expanded;
+    if (!expanded) {
+      await tick();
+      window.scrollBy(0, buttonEl.offsetTop - offsetTop);
+    }
+  };
 </script>
 
 <section class:collapsed={!expanded} class="content-section" id={section.id}>
   <slot />
   {#if index !== -1}
     <div class="expand-button-collapsed">
-      <ExpandButton content={showmore} {expanded} on:click={() => (expanded = !expanded)} />
+      <ExpandButton content={showmore} {expanded} on:click={onClickExpand} />
     </div>
   {/if}
 </section>

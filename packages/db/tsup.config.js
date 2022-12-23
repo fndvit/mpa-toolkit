3bbac70139b1a382b2e8f786712d7afe9d89db0e
Matt Osborn <mattjosborn@gmail.com>
2022-12-23 08:23:05 -0600

    bundle db in dev

--- packages/db/tsup.config.js
+++ packages/db/tsup.config.js
@@ -2,18 +2,9 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig(options => {
   const DEV = options.watch;
-  if (DEV) {
-    return {
-      entry: ['src'],
-      bundle: false,
-      sourcemap: 'inline',
-      loader: { '.json': 'copy' },
-      format: ['esm']
-    };
-  } else {
-    return {
-      entry: ['src/index.ts'],
-      format: ['esm']
-    };
-  }
+  return {
+    entry: ['src/index.ts'],
+    sourcemap: DEV ? 'inline' : false,
+    format: ['esm']
+  };
 });

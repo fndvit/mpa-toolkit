451badeeee640011eecfef5b5f88510c3ea871be
Matt Osborn <mattjosborn@gmail.com>
2022-12-29 22:18:09 -0600

    add pg tools to migration runner

--- packages/migration-runner/build.ts
+++ packages/migration-runner/build.ts
@@ -1,9 +1,12 @@
-import fs from 'fs-extra';
+import fs from 'fs';
+import { copy } from 'fs-extra';
 import { Listr } from 'listr2';
 import * as tsup from 'tsup';
 import projectRoot from '@mpa/utils/projectRoot';
 import { copyPrismaClientFiles } from '@mpa/utils/prisma/files';
 import shell from 'shelljs';
+import decompress from 'decompress';
+import fetch from 'node-fetch';
 
 const dist = 'dist';
 
@@ -11,7 +14,7 @@ const tasks = new Listr(
   [
     {
       title: 'Cleaning',
-      task: () => (fs.pathExistsSync(dist) ? fs.rmSync(dist, { recursive: true }) : undefined)
+      task: () => (fs.existsSync(dist) ? fs.rmSync(dist, { recursive: true }) : undefined)
     },
     {
       title: 'Building',
@@ -34,13 +37,35 @@ const tasks = new Listr(
     {
       title: 'Copying prisma files',
       task: async () => {
-        const root = await projectRoot();
-        return fs.copy(`${root}/packages/db/prisma`, `${dist}/prisma`, { recursive: true });
+        return copy(projectRoot('packages/db/prisma'), `${dist}/prisma`, { recursive: true });
       }
     },
     {
       title: 'Copying prisma client files',
       task: () => copyPrismaClientFiles(`${dist}/node_modules/.prisma/client`)
+    },
+    {
+      title: 'Copying PostgreSQL files',
+      task: async () => {
+        const cache = projectRoot('node_modules/.cache/pgdump-aws-lambda');
+
+        if (!fs.existsSync(cache) || !fs.readdirSync(cache).length) {
+          fs.mkdirSync(cache, { recursive: true });
+          const URL = 'https://github.com/jameshy/pgdump-aws-lambda/releases/download/v1.5.1/pgdump-aws-lambda.zip';
+          const buffer = await fetch(URL)
+            .then(res => res.arrayBuffer())
+            .then(arrayBuf => Buffer.from(arrayBuf));
+
+          await decompress(buffer, cache, {
+            filter: file => file.path.startsWith('bin/postgres-14.5/'),
+            map: file => ({
+              ...file,
+              path: file.path.replace('bin/postgres-14.5/', '')
+            })
+          });
+        }
+        await copy(cache, `${dist}/pg`, { recursive: true });
+      }
     }
   ]
   // {
--- packages/migration-runner/migration-runner.ts
+++ packages/migration-runner/migration-runner.ts
@@ -4,6 +4,7 @@ import { prismaCmd } from '@mpa/utils/prisma/cmd';
 import { logger } from '@mpa/log';
 import { getEnv } from '@mpa/env';
 import { execFileSync } from 'child_process';
+import path from 'path';
 
 export const env = getEnv({ DATABASE_URL: true });
 
@@ -11,33 +12,61 @@ const log = logger('migration-runner');
 // example cmd to invoke using aws cli:
 // aws lambda invoke --function-name AppStack-MigrationRunner07C61515-63HtCcSdD3K6 response.json
 
+const VALID_COMMANDS = ['seed', 'seed:dev', 'nuke', 'reset', 'deploy', 'sql_dump', 'sql_load'] as const;
+
+interface CommandPayload {
+  command: Exclude<typeof VALID_COMMANDS[number], 'sql_load'>;
+}
+
+interface SqlLoadPayload {
+  command: 'sql_load';
+  sql: string;
+}
+
+export type Payload = CommandPayload | SqlLoadPayload;
+
 export const handler: Handler = async event => {
   // Available commands are:
   //   deploy: create new database if absent and apply all migrations to the existing database.
   //   reset: delete existing database, create new one, and apply all migrations. NOT for production environment.
   // If you want to add commands, please refer to: https://www.prisma.io/docs/concepts/components/prisma-migrate
-  const command: string = event.command ?? 'deploy';
+
+  const payload = event as Payload;
+
+  if (!payload.command) throw new Error('No command specified');
+
+  if (!VALID_COMMANDS.includes(payload.command)) throw new Error(`Invalid command: ${payload.command}`);
 
   const db = initDatabase();
 
-  log.info(`Running migration command: ${command}`);
+  log.info(`Running migration command: ${payload.command}`);
 
-  if (command === 'seed') {
+  if (payload.command === 'seed') {
     const seeder = new ProdSeeder(db);
     await seeder.migrate();
-  } else if (command === 'seed:dev') {
+  } else if (payload.command === 'seed:dev') {
     const seeder = new DevSeeder(db);
     await seeder.seed();
-  } else if (command === 'nuke') {
+  } else if (payload.command === 'nuke') {
     const seeder = new ProdSeeder(db);
     await reset(db.prisma);
     await seeder.migrate();
-  } else if (command === 'reset') {
+  } else if (payload.command === 'reset') {
     await prismaCmd('migrate reset --force --skip-generate');
-  } else if (command === 'deploy') {
+  } else if (payload.command === 'deploy') {
     await prismaCmd('migrate deploy');
-  } else if (command === 'sql_dump') {
-    execFileSync('pg_dump', [env.DATABASE_URL]);
+  } else if (payload.command === 'sql_dump') {
+    execFileSync('./pg/pg_dump', [env.DATABASE_URL], {
+      env: {
+        LD_LIBRARY_PATH: path.resolve('./pg')
+      }
+    });
+  } else if (payload.command === 'sql_load') {
+    execFileSync('./pg/psql', [env.DATABASE_URL], {
+      env: {
+        LD_LIBRARY_PATH: path.resolve('./pg')
+      }
+    });
   }
 
   log.info(`Finished`);

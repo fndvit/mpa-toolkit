eea6a920d5b149cdc6d55bf85ffb20e690ba4e3d
Matt Osborn <mattjosborn@gmail.com>
2022-12-30 11:06:32 -0600

    docker for psql binaries

--- packages/migration-runner/scripts/build.ts
+++ packages/migration-runner/scripts/build.ts
@@ -5,9 +5,6 @@ import * as tsup from 'tsup';
 import projectRoot from '@mpa/utils/projectRoot';
 import { copyPrismaClientFiles } from '@mpa/utils/prisma/files';
 import shell from 'shelljs';
-import decompress from 'decompress';
-import fetch from 'node-fetch';
-import compose from 'docker-compose';
 
 const dist = 'dist';
 
@@ -46,17 +43,77 @@ const tasks = new Listr([
   },
   {
     title: 'Copying PostgreSQL files',
-    task: async () => {
-      const opts = {
-        composeOptions: ['-p migration-runner-build'],
-        config: ['scripts/docker-compose-pgbinaries.yml']
+    task: async (_, task) => {
+      // copy cached files if they exist
+      if (fs.existsSync('pg_binaries') && fs.readdirSync('pg_binaries').length > 0) {
+        task.output = 'Copying pre-existing files...';
+        copy('pg_binaries', `${dist}/pg`, { recursive: true });
+        task.output = 'Copied cached files';
+        return;
+      }
+
+      // otherwise get them from docker
+
+      task.output = 'Preparing...';
+      shell.exec('docker rm -f migrationrunnerbuild', { silent: true });
+      fs.rmSync('pg_binaries', { recursive: true, force: true });
+      fs.mkdirSync('pg_binaries');
+
+      task.output = 'Starting container...';
+      shell.exec(
+        `docker run \
+        --name migrationrunnerbuild \
+        -e POSTGRES_USER=prisma \
+        -e POSTGRES_PASSWORD=prisma \
+        -e POSTGRES_DB=mpa \
+        --platform linux/amd64 \
+        -v $(pwd)/pg_binaries:/pg_binaries \
+        -v $(pwd)/scripts/copy.sh:/scripts/copy.sh:ro \
+        -d \
+        postgres:13`,
+        { silent: true }
+      );
+
+      const removeContainer = () => shell.exec(`docker rm -f migrationrunnerbuild`, { silent: true });
+
+      const waitForContainer = async () => {
+        const startTime = Date.now();
+        const timeout = 60;
+
+        const elapsed = () => (Date.now() - startTime) / 1000;
+
+        while (elapsed() < timeout) {
+          const lastLogLine = shell.exec(`docker logs migrationrunnerbuild -n5`, { silent: true });
+
+          if (/database system is ready to accept connections/g.test(lastLogLine)) return true;
+
+          task.output = `Waiting for container to start... (${(timeout - elapsed()).toFixed(0)}s left)\n`;
+          task.output += lastLogLine.split('\n').slice(-3).join('\n');
+          await new Promise(resolve => setTimeout(resolve, 250));
+        }
+        return false;
       };
 
-      await compose.upOne('psql', opts);
-      await compose.exec('psql', 'bash /scripts/copy.sh', opts);
+      const ready = await waitForContainer();
 
+      if (!ready) {
+        task.output = 'Container failed to start';
+        removeContainer();
+        throw new Error('Container failed to start');
+      }
+
+      task.output = `Container ready. Copying files...`;
+      shell.exec(`docker exec migrationrunnerbuild bash /scripts/copy.sh`, { silent: true });
+
+      task.output = 'Removing container...';
+      removeContainer();
+
+      task.output = 'Copying files...';
       await copy('pg_binaries', `${dist}/pg`, { recursive: true });
-    }
+
+      task.output = '';
+    },
+    options: { persistentOutput: true }
   }
 ]);
 

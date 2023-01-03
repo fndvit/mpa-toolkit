b1e1e82b5376a3e6bcfc8d94d38226007aae6468
Matt Osborn <mattjosborn@gmail.com>
2023-01-03 00:35:32 -0600

    env file naming

--- packages/env/local.ts
+++ packages/env/local.ts
@@ -1,6 +1,9 @@
 import path from 'path';
 import * as dotenv from 'dotenv';
 
-const envFile = process.env.TEST ? '.env.test' : '.env';
+const envPath = (file: string) => path.join(process.cwd(), '../..', file);
 
-dotenv.config({ path: path.join(process.cwd(), '../..', envFile) });
+dotenv.config({ path: envPath('.env.local.base') });
+dotenv.config({ path: envPath('.env.local'), override: true });
+
+if (process.env.TEST) dotenv.config({ path: envPath('.env.test'), override: true });

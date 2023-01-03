014c88cf7961880c1411a9ca02269d992adbde22
Matt Osborn <mattjosborn@gmail.com>
2023-01-03 00:33:54 -0600

    up -> ts

---
+++ packages/db/test/db/up.ts
@@ -0,0 +1,15 @@
+import compose from 'docker-compose';
+
+const opts = {
+  composeOptions: ['-p mpa'],
+  config: ['docker-compose.yml']
+};
+
+async function initDb() {
+  compose.upOne('db-test', { ...opts, commandOptions: ['--force-recreate'] });
+  compose.exec('db-test', 'bash /data/load.sh', opts);
+}
+
+initDb()
+  .then(() => console.log('done'))
+  .catch(err => console.error(err));

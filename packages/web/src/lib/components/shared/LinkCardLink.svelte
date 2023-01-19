64893ed817796b96209139613e4b27db9e83443f
Matt Osborn <mattjosborn@gmail.com>
2023-01-19 13:11:40 -0600

    console log the error on link card errors

--- packages/web/src/lib/components/shared/LinkCardLink.svelte
+++ packages/web/src/lib/components/shared/LinkCardLink.svelte
@@ -29,6 +29,7 @@
       if (title && !card.title) card.title = title;
       if (image && !card.img) card.img = image;
     } catch (e) {
+      console.error(e);
       const msg = e instanceof HTTPError && (await e.response.json()).message;
       toaster.error('Error fetching URL' + (msg ? `: ${msg}` : ''));
     }

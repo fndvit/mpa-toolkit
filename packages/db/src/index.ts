9ef952cd6cfd0a08cb8d8bcab16998dba3e6b357
Matt Osborn <mattjosborn@gmail.com>
2023-01-19 12:04:01 -0600

    fix content block typings

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,6 +1,8 @@
 export { DBMixin, BaseDatabase } from './base';
 export { initDatabase, type MpaDatabase } from './db';
+export { ValidationError } from './validation';
 export * as Queries from './queries';
 export * from './types';
 export * from './lib';
 export { UserMixin } from './mixins/user';
+export { PrismaClientKnownRequestError } from '@prisma/client/runtime';

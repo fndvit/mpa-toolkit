733a1431570bd10cc5497423e92a9dddf4a1357c
Xaquín G.V <xocasgv@gmail.com>
2024-06-06 22:31:13 +0200

    feat: :sparkles: Add logos

--- packages/web/src/lib/svg/index.ts
+++ packages/web/src/lib/svg/index.ts
@@ -5,4 +5,7 @@ export { default as MPATH_W } from './MPATH_W.svg';
 export { default as UNEP } from './UNEP.svg';
 export { default as UoQ } from './UoQ.svg';
 export { default as WWF } from './WWF.svg';
+export { default as UdG } from './UdG.svg';
+export { default as ViT } from './ViT.svg';
+export { default as CRRI } from './CRRI.svg';
 export { default as file } from './file.svg';

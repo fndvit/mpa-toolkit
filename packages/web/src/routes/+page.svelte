d4f0135618a5867f8f3d4d2194ea4597017568a7
Xaquín G.V <xocasgv@gmail.com>
2024-06-06 23:35:28 +0200

    fix: :bug: Fix logo layout

--- packages/web/src/routes/+page.svelte
+++ packages/web/src/routes/+page.svelte
@@ -32,13 +32,15 @@
     </h4>
     <h5>UNEP, in partnership with</h5>
     <div class="partners-grid">
-      <InlineSvgLink href="https://www.aics.gov.it/language/en/" svg="IAFDC" newTab />
-      <InlineSvgLink href="https://www.uq.edu.au" svg="UoQ" newTab />
+      <div class="partners-grid-1">
+        <InlineSvgLink href="https://www.aics.gov.it/language/en/" svg="IAFDC" newTab />
+        <InlineSvgLink href="https://www.uq.edu.au" svg="UoQ" newTab />
+      </div>
       <div class="partners-grid-2">
         <InlineSvgLink href="https://www.nature.org/" svg="TNC" newTab />
         <InlineSvgLink href="https://wwf.panda.org" svg="WWF" newTab />
       </div>
-      <div class="partners-grid-2">
+      <div class="partners-grid-3">
         <InlineSvgLink href="https://coralreefrescueinitiative.org/" svg="CRRI" newTab />
         <InlineSvgLink href="https://www.udg.edu/en/" svg="UdG" newTab />
         <InlineSvgLink href="https://www.fundaciovit.org/" svg="ViT" newTab />
@@ -95,10 +97,18 @@
 
   .partners-grid {
     display: grid;
-    grid-template-columns: 87px 148px 1fr;
+    grid-template-rows: auto;
     align-items: center;
     gap: 45px;
     row-gap:24px;
+  }
+
+  .partners-grid-1 {
+    display: inherit;
+    grid-template-columns: 87px 148px 1fr;
+    align-items: inherit;
+    gap: inherit;
+    color: inherit;
   }
 
   .partners-grid-2 {
@@ -236,9 +246,12 @@
     }
 
     .partners-grid {
-      grid-template-columns: 87px 148px;
       grid-template-rows: auto;
       margin-top: 1rem;
+    }
+
+    .partners-grid-1 {
+      grid-template-columns: 87px 148px;
     }
 
     .partners-grid-2 {
